6f2cda36dda40bdc419731cd26aea391535e5513
Rosario Alonso <79224753+margarita1973@users.noreply.github.com>
2024-09-19 00:49:59 +0000

    complete exercise

---
+++ clase.js
@@ -0,0 +1,13 @@
+let age = 18;
+if(age <18){
+    console.log("no puedes"); 
+}
+else {console.log("puedes");
+}
+
+//ternary
+(age>18)? console.log("nopuedes"):console.log("puedes");
+
+
+let names=['martin', 'camila', 'laura', "luis"];
+names.forEach(())
